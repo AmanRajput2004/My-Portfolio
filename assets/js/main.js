796ce9ca9a5aa3c9ede9d8b1f8123603ed5c8bab
Aman Singh <aman.it360@gmail.com>
2024-09-04 07:04:26 +0530

    Files Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -203,13 +203,7 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-// Google recaptcha
-window.onload = function() { 
-    var el = document.getElementById('g-recaptcha-response'); 
-    if (el) { 
-      el.setAttribute('required', 'required'); 
-    } 
-  }
+
 
 // validate all field in the contact form
 document.getElementById("check").onclick = function() {
